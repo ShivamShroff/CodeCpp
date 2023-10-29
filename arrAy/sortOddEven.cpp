5a02c1e4d155ac07c6d7c32ab30ba43483895f93
Shivam Kumar Gaurav <shivamshroff111@gmail.com>
2023-10-29 16:13:07 +0530

    sortOddEven uploaded

---
+++ arrAy/sortOddEven.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortOddEven(vector<int>&v){
+    int leftPtr = 0;
+    int rightPtr = v.size()-1;
+
+    while(leftPtr < rightPtr){
+
+        if(v[leftPtr] % 2 == 1 && v[rightPtr] % 2 == 0){
+            swap(v[leftPtr],v[rightPtr]);
+            leftPtr++;
+            rightPtr--;
+        }
+
+        if(v[leftPtr] % 2 == 0){
+            leftPtr++;
+        }
+        if(v[rightPtr] % 2 == 1){
+            rightPtr--;
+        }
+        
+    }
+    return;
+}
+int main(){
+    int numbers;
+    cin >> numbers;
+
+    vector<int>v;
+
+    for(int i=0; i<numbers;  i++){
+        int elements; cin >> elements;
+        v.push_back(elements);
+    }
+
+    sortOddEven(v);
+
+    for(int i=0; i<numbers; i++){
+        cout << v[i] <<" ";
+        
+    }
+    cout << endl;
+
+    return 0;
+}
