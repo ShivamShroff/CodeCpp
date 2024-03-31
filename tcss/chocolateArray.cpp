b7ca5ac92f035368eaabcda39ad52b05ec1b1d62
Shivam Kumar Gaurav <shivamshroff111@gmail.com>
2024-03-31 19:56:58 +0530

    pushZeroInLast Question

---
+++ tcss/chocolateArray.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pushZeroLast(int arr[],int n){
+    int count = 0; //it will clear your confusion
+    for(int i=0; i<n; i++){
+        if(arr[i] != 0){
+            arr[count] = arr[i];
+            count++;
+        }
+    }
+    for(int i=count; i<n; i++){
+        arr[i] = 0;
+    }
+    for(int i=0; i<n; i++){
+        cout << arr[i] <<" ";
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    pushZeroLast(arr,n);
+
+    return 0;
+
+}
